177ddc22588993259e04a248bece2bbaa2abd739
witch641 <130225027+witch641@users.noreply.github.com>
2024-05-02 18:05:40 +0800

    main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const resumeInfo = getResumeInfo();
 
 // 与GPT进行聊天的函数
 async function chat(jobDescription) {
-  const askMessage = `你好，这是我的简历：${resumeInfo}，这是我所应聘公司的要求：${jobDescription}。我希望您能帮我直接给HR写一个礼貌专业的求职新消息，要求能够用专业的语言将简历中的技能结合应聘工作的描述，来阐述自己的优势，尽最大可能打动招聘者。并且请您始终使用中文来进行消息的编写,开头是招聘负责人。这是一封完整的求职信，不要包含求职信内容以外的东西，例如“根据您上传的求职要求和个人简历，我来帮您起草一封求职邮件：”这一类的内容，以便于我直接自动化复制粘贴发送，例外，我的名字是刘思忆，字数控制在80字左右为宜`;
+  const askMessage = `你好，这是我的简历：${resumeInfo}，这是我所应聘公司的要求：${jobDescription}。我希望您能帮我直接给HR写一个礼貌专业的求职新消息，要求能够用专业的语言将简历中的技能结合应聘工作的描述，来阐述自己的优势，尽最大可能打动招聘者。并且请您始终使用中文来进行消息的编写,开头是招聘负责人。这是一封完整的求职信，不要包含求职信内容以外的东西，例如“根据您上传的求职要求和个人简历，我来帮您起草一封求职邮件：”这一类的内容，以便于我直接自动化复制粘贴发送，另外，我的名字是刘思忆，字数控制在80字左右为宜`;
   try {
     const completion = await openai.chat.completions.create({
       messages: [
@@ -118,7 +118,7 @@ async function main(url, browserType) {
 }
 
 const url =
-  "https://www.zhipin.com/web/geek/job-recommend?ka=header-job-recommend";
+  "https://www.zhipin.com/web/geek/job-recommend?salary=403";
 const browserType = "chrome";
 
 main(url, browserType);
